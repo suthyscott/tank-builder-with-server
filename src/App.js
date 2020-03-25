883a2f03416394e5b5d226d6ef8e659a23c21298
Scott Sutherland <scottsutherland@Scotts-MacBook-Pro.local>
2020-03-24 18:48:44 -0600

    project done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,149 @@
 import React from 'react';
-import logo from './logo.svg';
+import tanksArr from './Tanks'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component {
+  constructor(){
+    super()
+
+    this.state = {
+      tanks: tanksArr,
+      name: '',
+      gun: {
+        production: '',
+        modified: null
+      },
+      crew: null,
+      lovedByScott: false,
+      img: '',
+      coolTankRating: null,
+      wars: []
+    }
+  }
+
+  handleUpdateName = val => {
+    this.setState({
+      name: val
+    })
+  }
+  handleUpdateGun = val => {
+    this.setState({
+      gun: {
+        production: val
+      }
+    })
+  }
+  handleUpdateCrew= val => {
+    this.setState({
+      crew: val
+    })
+  }
+  handleUpdateImage = val => {
+    this.setState({
+      img: val
+    })
+  }
+  handleUpdateCoolTankRating = val => {
+    this.setState({
+      coolTankRating: val
+    })
+  }
+  handleUpdateWars = val => {
+    const wars = val.split(',')
+    this.setState({
+      wars
+    })
+  }
+
+  handleAddTank = () => {
+    const {name, gun, crew, lovedByScott, coolTankRating, img, wars} = this.state
+    this.setState({
+      tanks: [...this.state.tanks, {
+        name,
+        gun,
+        crew,
+        lovedByScott,
+        coolTankRating,
+        img, 
+        wars
+      }]
+    })
+
+    this.setState({
+      name: '',
+      gun: {
+        production: '',
+        modified: null
+      },
+      crew: 0,
+      lovedByScott: false,
+      img: '',
+      coolTankRating: 0,
+      wars: []
+    })
+  }
+  
+  render(){
+    console.log(this.state)
+
+    const tanksDisplay = this.state.tanks.map(tank => {
+      // console.log(tank)
+      return <div className='tank-info'>
+        <div id='title-pic'>
+          <h1>{tank.name}</h1> <img className='tank-image' src={tank.img} />
+        </div>
+
+        <p>{tank.gun.production}</p>
+        <p>{tank.crew}</p>
+        <p>Scott loves it: {tank.lovedByScott ? 'True' : 'Nope'}</p>
+        <p>On a scale of Sick-Nasty to Laaaaaame this tank is a {tank.coolTankRating}</p>    
+        <p>{tank.wars.map(e => <span className='war'>{e}, </span>)} </p>   
+      </div>
+    })
+
+    return (
+    <main>
+      <h1>Scott's Legendary Tank Builder</h1>
+
+      <section>
+        <input 
+        placeholder='Name'
+        onChange={e => this.handleUpdateName(e.target.value)}
+        value={this.state.name}/>
+        <input 
+        placeholder='Gun'
+        onChange={e => this.handleUpdateGun(e.target.value)}
+        value={this.state.gun.production}/>
+        <input 
+        placeholder='Crew'
+        onChange={e => this.handleUpdateCrew(e.target.value)}
+        type='number'
+        value={this.state.crew}/>
+        <input 
+        placeholder='Sick tank pic'
+        onChange={e => this.handleUpdateImage(e.target.value)}
+        value={this.state.img}/>
+        <input 
+        placeholder='How cool is it?'
+        onChange={e => this.handleUpdateCoolTankRating(e.target.value)}
+        type='number'
+        value={this.state.coolTankRating}/>
+        <input 
+        placeholder='Wars (Separate by comma)'
+        onChange={e => this.handleUpdateWars(e.target.value)}
+        />
+
+        <button onClick={() => this.handleAddTank()}>Add Tank</button>
+      </section>
+      
+
+      <div id='tanks-display'>
+        {tanksDisplay}
+      </div>
+    </main>
   );
+  }
+  
 }
 
 export default App;
---
+++ src/Tanks.js
@@ -0,0 +1,62 @@
+module.exports = [
+    {
+      name: 'M4 Sherman',
+      gun: {
+        production: '76 mm gun M1A1',
+        modified: '105 mm howitzer M4'
+      },
+      crew: 5,
+      wars: ['World War II','Indonesian National Revolution','Greek Civil War','1948 Arab–Israeli War','Korean War','Cuban Revolution','Vietnam War','Revolución Libertadora','Suez Crisis','1958 Lebanon crisis','Nicaraguan Revolution','Indo-Pakistani War of 1965','Six-Day War', 'Indo-Pakistani War of 1971','Yom Kippur War','Lebanese Civil War','Uganda–Tanzania War','Iran–Iraq War'],
+      lovedByScott: true,
+      coolTankRating: 10,
+      img:'https://previews.123rf.com/images/zhukovsky/zhukovsky1703/zhukovsky170300415/74664768-old-bethpage-new-york-may-22-2016-the-m4-sherman-tank-from-the-museum-of-american-armor-during-world.jpg'
+    },
+    {
+      name: 'M18 Hellcat',
+      gun: {
+        production: '76 mm gun M1A1',
+        modified: null
+      },
+      crew: 5,
+      wars: ['World War II', 'Korean War', 'Yugoslav Wars'],
+      lovedByScott: true,
+      coolTankRating: 10,
+      img:'https://armyhistory.org/wp-content/uploads/2017/08/3.-801st-TD-3-1024x527.png'
+    },
+    {
+      name: 'Tiger I',
+      gun: {
+        production: '8.8 cm KwK 36 L/56',
+        modified: null
+      },
+      crew: 5,
+      wars: ['World War II'],
+      lovedByScott: false,
+      coolTankRating: 10,
+      img:'https://blog.tiger-tank.com/wp-content/uploads/2018/04/0053-C2.jpg'
+    },
+    {
+      name: 'T-34',
+      gun: {
+        production: '76.2 mm F-34 tank gun',
+        modified: '(T-34-85: 85 mm ZiS-S-53 or D-5T gun)'
+      },
+      crew: 5,
+      wars: ['World War II', 'Like all of them'],
+      lovedByScott: false,
+      coolTankRating: 10,
+      img:'https://i.ytimg.com/vi/cenrTGcdM1c/maxresdefault.jpg'
+    },
+    {
+      name: 'KV-2',
+      gun: {
+        production: '152 mm M-10T howitzer',
+        modified: null
+      },
+      crew: 5,
+      wars: ['World War II', 'Like all of them'],
+      lovedByScott: true,
+      coolTankRating: 10,
+      img:'https://www.tanks-encyclopedia.com/wp-content/uploads/2015/04/german-inspect-kv2.jpg'
+    }
+  ]
